8d87818be7f91656a93966773bef490654eadd28
Tyler Thompson <tyler@tylerpaulthompson.com>
2016-03-19 23:48:24 -0700

    Update Podfile for Cocoapods 1.0.0b6 compatibility; remove example tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,50 +1,11 @@
-// https://github.com/Quick/Quick
-
 import Quick
 import Nimble
 import SwiftECP
 
-class TableOfContentsSpec: QuickSpec {
+class SwiftECPSpec: QuickSpec {
     override func spec() {
-        describe("these will fail") {
+        describe("TODO: write tests") {
 
-            it("can do maths") {
-                expect(1) == 2
-            }
-
-            it("can read") {
-                expect("number") == "string"
-            }
-
-            it("will eventually fail") {
-                expect("time").toEventually( equal("done") )
-            }
-            
-            context("these will pass") {
-
-                it("can do maths") {
-                    expect(23) == 23
-                }
-
-                it("can read") {
-                    expect("🐮") == "🐮"
-                }
-
-                it("will eventually pass") {
-                    var time = "passing"
-
-                    dispatch_async(dispatch_get_main_queue()) {
-                        time = "done"
-                    }
-
-                    waitUntil { done in
-                        NSThread.sleepForTimeInterval(0.5)
-                        expect(time) == "done"
-
-                        done()
-                    }
-                }
-            }
         }
     }
 }
